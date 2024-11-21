1c85e39a5a48774458fb000506fed1d3e20c76c8
Santiago29-6 <chavarrokevin29@gmail.com>
2024-11-21 14:30:36 -0500

    Se añade anotacion que faltaba
    - Se añade anotación que faltaba en la parte de repositorios
    - Se añade nuevas funciones en servicios
    - Se corrigue error en el controller

--- src/main/java/com/proyecto/proyecto/repository/EstadoRepository.java
+++ src/main/java/com/proyecto/proyecto/repository/EstadoRepository.java
@@ -1,8 +1,10 @@
 package com.proyecto.proyecto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.proyecto.proyecto.model.Estado;
 
+@Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long>{
 }
