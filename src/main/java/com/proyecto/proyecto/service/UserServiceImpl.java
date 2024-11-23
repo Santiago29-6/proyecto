30cdec2421f1b42eb642e774ccd6d5ce03c045a5
Santiago29-6 <chavarrokevin29@gmail.com>
2024-11-23 18:17:45 -0500

    Complemtacion de configuraciones de seguridad
    - Se agregan el resto de configuraciones de seguridad

--- src/main/java/com/proyecto/proyecto/service/UserServiceImpl.java
+++ src/main/java/com/proyecto/proyecto/service/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.proyecto.proyecto.service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.proyecto.proyecto.model.Role;
 import com.proyecto.proyecto.model.User;
 import com.proyecto.proyecto.repository.UserRepository;
+import com.proyecto.proyecto.security.jwt.JwtProvider;
+
+import jakarta.transaction.Transactional;
 
 public class UserServiceImpl implements UserService{
 
@@ -17,13 +22,40 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtProvider jwtProvider;
+
     @Override
     public User saveUser(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.USER);
         user.setFechaRegistro(LocalDateTime.now());
         User userCreated = userRepository.save(user);
+
+        String jwt = jwtProvider.generateToken(userCreated);
+        userCreated.setToken(jwt);
         
         return userCreated;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional
+    @Override
+    public void changeRole(Role newRole, String username) {
+        userRepository.updateUserRole(username, newRole);
+    }
+
+    @Override
+    public User findByUsernameReturnToken(String username){
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("El usuario no existe: " + username));
+        
+        String jwt = jwtProvider.generateToken(user);
+        user.setToken(jwt);
+        return user;
     }
 }
