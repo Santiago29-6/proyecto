0e5578ceca81248e731bdba8235591a2dda95430
Santiago29-6 <chavarrokevin29@gmail.com>
2025-01-30 23:48:07 -0500

    feat: add dto in service user

--- src/main/java/com/proyecto/proyecto/service/user/UserService.java
+++ src/main/java/com/proyecto/proyecto/service/user/UserService.java
@@ -1,21 +1,19 @@
 package com.proyecto.proyecto.service.user;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.proyecto.proyecto.dto.request.UserRequestDTO;
 import com.proyecto.proyecto.dto.response.UserResponseDTO;
 import com.proyecto.proyecto.enums.*;
-import com.proyecto.proyecto.model.User;
 
 @Service
 public interface UserService {
 
     UserResponseDTO saveUser(UserRequestDTO userRequestDTO);
 
-    Optional<User> findByUsername(String username);
+    UserResponseDTO findByUsername(String username);
 
     void changeRole(Role newRole, String username);
 
@@ -25,8 +23,8 @@ public interface UserService {
 
     boolean deleteUser(Long id);
 
-    Optional<User> findUserById(Long id);
+    UserResponseDTO findUserById(Long id);
 
-    Optional<User> checkIfUsernameExists(String username);
+    UserResponseDTO checkIfUsernameExists(String username);
 
 }
